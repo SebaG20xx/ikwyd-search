e5dd26864ba50f10157cf556bce05f4345823cdd
Sebastián Guevara <82485346+SebaG20xx@users.noreply.github.com>
2022-06-16 21:47:33 -0400

    Contador Añadido
    También si fue encontrada una ip o no

--- ikwyd.py
+++ ikwyd.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+cont = 0
 listaips = open('ips.txt')
 registro = open('ipsdetectadas.txt', 'w')
 URL = "https://iknowwhatyoudownload.com/en/peer/?ip="
+def contarlineas(archivo):
+    file = open(archivo,"r")
+    n = 0
+    for linea in file:
+        n+=1
+    file.close()
+    return n
+cantidad_lineas = contarlineas("ips.txt")
 for ip in listaips:
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
     URLIP = URL + ip
@@ -12,4 +21,9 @@ for ip in listaips:
     if out != []:
         print("IP Encontrada: " + str(ip))
         registro.write(ip)
+    else:
+        print("IP no Encontrada: " + str(ip))
+    cont += 1
+    restantes = cantidad_lineas - cont
+    print("Quedan " + str(restantes) + " ips restantes")
 print("Terminado")
